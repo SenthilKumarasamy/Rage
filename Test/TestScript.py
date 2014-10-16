9e51070498dfcc77254d48ee2632bbb589e250e1
SenthilKumarasamy <askforsenthil@gmail.com>
2014-10-16 12:29:44 -0700

    TestScript is modified. A variable Ctol is added.

--- Test/TestScript.py
+++ Test/TestScript.py
@@ -8,17 +8,18 @@ from EX7 import Test7
 from EX8 import Test8
 from EX9 import Test9
 from EX10 import Test10
+Ctol=1e-5
 List=[]
-List.append(Test1(1e-5))
-List.append(Test2(1e-5))
-List.append(Test3(1e-5))
-List.append(Test4(1e-5))
-List.append(Test5(1e-5))
-List.append(Test6(1e-5))
-List.append(Test7(1e-5))
-List.append(Test8(1e-5))
-List.append(Test9(1e-5))
-List.append(Test10(1e-5))
+List.append(Test1(Ctol))
+List.append(Test2(Ctol))
+List.append(Test3(Ctol))
+List.append(Test4(Ctol))
+List.append(Test5(Ctol))
+List.append(Test6(Ctol))
+List.append(Test7(Ctol))
+List.append(Test8(Ctol))
+List.append(Test9(Ctol))
+List.append(Test10(Ctol))
 
 for i in List:
     print i.TestResult
